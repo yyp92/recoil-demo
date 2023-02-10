d8255d78f4b56523e9ea98cf39ae38162f9f76f6
yyp <yangyipeng@joyame.com>
2023-02-10 17:27:00 +0800

    feat:html2canvas-生成图片插件学习

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,53 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Test from './components'
+import generateShareImage from './components/Ttml2canvas'
 
 function App() {
+  const hanldeCreateImg = () => {
+    generateShareImage('recoil-wrap', 'preview')
+  }
 
   return (
     <div className="App">
       <h1>Recoil demo</h1>
       
-      <Test />
+      <div id="recoil-wrap">
+        <Test />
+      </div>
+      
+      {/* 生成图片 */}
+      <div
+        style={{
+          border: '1px dashed red',
+          width: '800px',
+          margin: '20px auto',
+          borderRadius: '20px'
+        }}
+      >
+        <button
+          style={{marginTop: '20px'}}
+          onClick={hanldeCreateImg}
+        >生成图片</button>
+
+        <div
+          style={{
+            width: '100%',
+            height: '200px',
+            padding: '20px',
+            boxSizing: 'border-box'
+          }}
+        >
+          <h3>预览</h3>
+          <div
+            id="preview"
+            style={{
+              border: '1px dashed green',
+              padding: '5px'
+            }}
+          ></div>
+        </div>
+      </div>
     </div>
   )
 }
