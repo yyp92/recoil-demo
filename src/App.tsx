c8116cb254d5a2fbf5b5148b0f1ad1d83e68d9b5
yyp <yangyipeng@joyame.com>
2023-02-13 10:37:10 +0800

    feat:去掉与recoil无关的代码

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Test from './components'
-import generateShareImage from './components/Ttml2canvas'
 
 function App() {
-  const hanldeCreateImg = () => {
-    generateShareImage('recoil-wrap', 'preview')
-  }
 
   return (
     <div className="App">
@@ -15,39 +11,6 @@ function App() {
       
       <div id="recoil-wrap">
         <Test />
-      </div>
-      
-      {/* 生成图片 */}
-      <div
-        style={{
-          border: '1px dashed red',
-          width: '800px',
-          margin: '20px auto',
-          borderRadius: '20px'
-        }}
-      >
-        <button
-          style={{marginTop: '20px'}}
-          onClick={hanldeCreateImg}
-        >生成图片</button>
-
-        <div
-          style={{
-            width: '100%',
-            height: '200px',
-            padding: '20px',
-            boxSizing: 'border-box'
-          }}
-        >
-          <h3>预览</h3>
-          <div
-            id="preview"
-            style={{
-              border: '1px dashed green',
-              padding: '5px'
-            }}
-          ></div>
-        </div>
       </div>
     </div>
   )
